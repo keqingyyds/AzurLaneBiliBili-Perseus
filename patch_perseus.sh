0eea32f50c3ba58d74a21046354699731203dbda
keqingyyds <105032048+keqingyyds@users.noreply.github.com>
2025-02-16 04:52:32 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -29,7 +29,7 @@ if [ ! -f "com.bilibili.AzurLane.apk" ]; then
 
     # eg: wget "your download link" -O "your packge name.apk" -q
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
-    wget https://pkg.biligame.com/games/blhx_8.2.1_0820_1_20240830_041443_51682.apk -O com.bilibili.AzurLane.apk -q
+    wget https://go.8546512.com/down37/app/bilanhangxian.apk -O com.bilibili.AzurLane.apk -q
     echo "apk downloaded !"
     
     # if you can only download .xapk file uncomment 2 lines below. (delete the '#')
@@ -40,22 +40,23 @@ fi
 # Download Perseus
 if [ ! -d "Perseus" ]; then
     echo "Downloading Perseus"
-    git clone https://github.com/IceEst/Azurlane_libSkin
+    git clone https://github.com/keqingyyds/Perseus/
 fi
 
 echo "Decompile Azur Lane apk"
 java -jar apktool.jar -q -f d com.bilibili.AzurLane.apk
 
 echo "Copy Perseus libs"
-cp -r Azurlane_libSkin/. com.bilibili.AzurLane/lib/
+cp -r Perseus/. com.bilibili.AzurLane/lib/
 
 echo "Patching Azur Lane with Perseus"
 oncreate=$(grep -n -m 1 'onCreate' com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
 sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
-sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"JMBQ\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Perseus\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" com.bilibili.AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
 
 echo "Build Patched Azur Lane apk"
 java -jar apktool.jar -q -f b com.bilibili.AzurLane -o build/com.bilibili.AzurLane.patched.apk
 
 echo "Set Github Release version"
-echo "PERSEUS_VERSION=$(echo JMBQ)" >> $GITHUB_ENV
+s=($(./apkeep -a com.bilibili.AzurLane -l))
+echo "PERSEUS_VERSION=$(echo ${s[-1]})" >> $GITHUB_ENV
